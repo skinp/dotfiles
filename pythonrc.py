5d24367ee7c5c7eb0a0f07cc146e45409e9b4b9f
Patrick Pelletier <pp.pelletier@gmail.com>
2014-02-13 11:19:50 -0800

    Add pythonrc.py

---
+++ pythonrc.py
@@ -0,0 +1,13 @@
+import os
+import re
+import sys
+import time
+import random
+
+try:
+    import readline
+except ImportError:
+    print "Module readline not available."
+else:
+    import rlcompleter
+    readline.parse_and_bind("tab: complete")
